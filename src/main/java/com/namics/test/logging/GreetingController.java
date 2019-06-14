e535f23303e61339435e4c2281a6406a681142fd
Cornel Eberle <cornel.eberle@namics.com>
2019-06-14 17:03:04 +0200

    Set up Spring Boot application

---
+++ src/main/java/com/namics/test/logging/GreetingController.java
@@ -0,0 +1,25 @@
+package com.namics.test.logging;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(GreetingController.class);
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        LOGGER.info("Got greeting: {}", name);
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
